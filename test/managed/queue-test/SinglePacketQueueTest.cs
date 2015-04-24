e1372135bd6dc340f3f73ef2458b56b3ee4dc663
Aurojit <apanda@cs.berkeley.edu>
2015-04-24 10:12:30 -0700

    Refactor to add more tests

--- test/managed/queue-test/SinglePacketQueueTest.cs
+++ test/managed/queue-test/SinglePacketQueueTest.cs
@@ -19,82 +19,42 @@ public class Packet {
   }
 }
 
-public class SinglePacketTest {
-#if __MonoCS__
-  [DllImport("libc.so.6")]
-  private static extern int sched_getcpu();
-    
-  [DllImport("libc.so.6", SetLastError = true)]
-  private static extern int sched_setaffinity(int pid, IntPtr cpusetsize, ulong[] cpuset);
-#else
-  [DllImport("kernel32.dll")]
-  static extern int GetCurrentProcessorNumber();
-#endif
+public class SinglePacketTestAllocate {
 
   protected internal ConcurrentQueue<Packet> queue_;
 
   private int producerCore_;
   private int consumerCore_;
-  private int nbufs_;
-  public SinglePacketTest(int producerCore, int consumerCore, int nbuffers) { 
+  public SinglePacketTestAllocate(int producerCore, int consumerCore) { 
     queue_ = new ConcurrentQueue<Packet>();
     producerCore_ = producerCore;
     consumerCore_ = consumerCore;
-    nbufs_ = nbuffers;
-  }
-
-  protected void SetAffinity (int core) {
-    #if __MonoCS__
-    ulong processorMask = 1UL << core;
-    sched_setaffinity(0, new IntPtr(sizeof(ulong)), new [] {processorMask});
-    #else
-    long cpuMask = 1L << core;
-    Thread.BeginThreadAffinity();
-    #pragma warning disable 618
-    int osThreadId = AppDomain.GetCurrentThreadId();
-    #pragma warning restore 618
-    ProcessThread thread =  Process.GetCurrentProcess().Threads.Cast<ProcessThread>()
-                                       .Where(t => t.Id == osThreadId).Single();
-    thread.ProcessorAffinity = new IntPtr(cpuMask);
-    #endif
-  }
-
-  protected int GetCurrentCpu () {
-    #if __MonoCS__
-    return sched_getcpu();
-    #else
-    return GetCurrentProcessorNumber();
-    #endif
   }
 
   protected void ProducerStart() {
-    SetAffinity(producerCore_);
-    //Packet[] packets = new Packet[nbufs_];
+    SysUtils.SetAffinity(producerCore_);
     Stopwatch stopwatch = new Stopwatch();
     stopwatch.Start();
-    long lastSec = stopwatch.ElapsedMilliseconds / 1000;
+    long lastSec = SysUtils.GetSecond(stopwatch);
     long count = 0;
     long absCount = 0;
-    long buf = 0;
     while (true) {
-      long currSec = stopwatch.ElapsedMilliseconds / 1000;
+      long currSec = SysUtils.GetSecond(stopwatch);
       queue_.Enqueue(new Packet(absCount));
-      //buf = (buf + 1) % nbufs_;
       count++;
       absCount++;
       if (currSec != lastSec) {
         lastSec = currSec;
-        // Console.WriteLine locks, so let us further reduce contention here
         count = 0;
       }
     }
   }
 
   protected void ConsumerStart() {
-    SetAffinity(consumerCore_);
+    SysUtils.SetAffinity(consumerCore_);
     Stopwatch stopwatch = new Stopwatch();
     stopwatch.Start();
-    long lastSec = stopwatch.ElapsedMilliseconds / 1000;
+    long lastSec = SysUtils.GetSecond(stopwatch);
     long count = 0;
     long received = 0;
     while (true) {
@@ -103,10 +63,10 @@ public class SinglePacketTest {
         count++;
         received = pkt.id;
       }
-      long currSec = stopwatch.ElapsedMilliseconds / 1000;
+      long currSec = SysUtils.GetSecond(stopwatch);
       if (currSec != lastSec) {
         lastSec = currSec;
-        Console.WriteLine("Consume " + GetCurrentCpu() + " " + count + " " + received);
+        Console.WriteLine("Consume " + SysUtils.GetCurrentCpu() + " " + count + " " + received);
         count = 0;
       }
     }
@@ -130,7 +90,7 @@ public class QueueTest {
     #else
     Console.WriteLine("Windows");
     #endif
-    SinglePacketTest sp = new SinglePacketTest(0, 1, 100);
+    SinglePacketTestAllocate sp = new SinglePacketTestAllocate(0, 1);
     sp.Start();
   }
 }
