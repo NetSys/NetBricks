15a8f8916cf95eeffef9917a91c3c56a538d8e50
Aurojit <apanda@cs.berkeley.edu>
2015-04-24 01:13:17 -0700

    Queue test

---
+++ test/managed/queue-test/QueueTest.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+#if (!__MonoCS__)
+using System.Linq;
+#endif
+
+namespace E2D2 {
+public class Packet {
+  Int64 id_;
+  public Int64 id 
+  {
+      get {return id_;}
+  }
+  public Packet(Int64 id) {
+    id_ = id;
+  }
+}
+
+public class SinglePacketTest {
+#if __MonoCS__
+  [DllImport("libc.so.6")]
+  private static extern int sched_getcpu();
+    
+  [DllImport("libc.so.6", SetLastError = true)]
+  private static extern int sched_setaffinity(int pid, IntPtr cpusetsize, ulong[] cpuset);
+#else
+  [DllImport("kernel32.dll")]
+  static extern int GetCurrentProcessorNumber();
+#endif
+
+  protected internal ConcurrentQueue<Packet> queue_;
+
+  private int producerCore_;
+  private int consumerCore_;
+  public SinglePacketTest(int producerCore, int consumerCore) { 
+    queue_ = new ConcurrentQueue<Packet>();
+    producerCore_ = producerCore;
+    consumerCore_ = consumerCore;
+  }
+
+  protected void SetAffinity (int core) {
+    #if __MonoCS__
+    ulong processorMask = 1UL << core;
+    sched_setaffinity(0, new IntPtr(sizeof(ulong)), new [] {processorMask});
+    #else
+    long cpuMask = 1L << core;
+    Thread.BeginThreadAffinity();
+    #pragma warning disable 618
+    int osThreadId = AppDomain.GetCurrentThreadId();
+    #pragma warning restore 618
+    ProcessThread thread =  Process.GetCurrentProcess().Threads.Cast<ProcessThread>()
+                                       .Where(t => t.Id == osThreadId).Single();
+    thread.ProcessorAffinity = new IntPtr(cpuMask);
+    #endif
+  }
+
+  protected int GetCurrentCpu () {
+    #if __MonoCS__
+    return sched_getcpu();
+    #else
+    return GetCurrentProcessorNumber();
+    #endif
+  }
+
+  protected void ProducerStart() {
+    SetAffinity(producerCore_);
+    Stopwatch stopwatch = new Stopwatch();
+    stopwatch.Start();
+    long lastSec = stopwatch.ElapsedMilliseconds / 1000;
+    long count = 0;
+    while (true) {
+      long currSec = stopwatch.ElapsedMilliseconds / 1000;
+      queue_.Enqueue(new Packet(currSec + count));
+      count++;
+      if (currSec != lastSec) {
+        lastSec = currSec;
+        Console.WriteLine("Produce " + GetCurrentCpu() + " " + count);
+        count = 0;
+      }
+    }
+  }
+
+  protected void ConsumerStart() {
+    SetAffinity(consumerCore_);
+    Stopwatch stopwatch = new Stopwatch();
+    stopwatch.Start();
+    long lastSec = stopwatch.ElapsedMilliseconds / 1000;
+    long count = 0;
+    while (true) {
+      Packet pkt;
+      if (queue_.TryDequeue(out pkt)) {
+        count++;
+      }
+      long currSec = stopwatch.ElapsedMilliseconds / 1000;
+      if (currSec != lastSec) {
+        lastSec = currSec;
+        Console.WriteLine("Consume " + GetCurrentCpu() + " " + count);
+        count = 0;
+      }
+    }
+  }
+
+  public void Start() {
+    Thread producer = new Thread(new ThreadStart(this.ProducerStart));
+    Thread consumer = new Thread(new ThreadStart(this.ConsumerStart));
+    producer.Start();
+    consumer.Start();
+    producer.Join();
+    consumer.Join();
+  }
+
+}
+
+public class QueueTest {
+  public static void Main (string[] args) {
+    #if __MonoCS__
+    Console.WriteLine("Decided this is Mono");
+    #else
+    Console.WriteLine("Windows");
+    #endif
+    SinglePacketTest sp = new SinglePacketTest(0, 1);
+    sp.Start();
+  }
+}
+}
