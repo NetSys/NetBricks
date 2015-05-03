bf4c3e357b4f28804e220102e88a9b9c9ed645b2
Aurojit <apanda@cs.berkeley.edu>
2015-05-02 21:36:37 -0700

    IP lookup fixes and tests

--- test/managed/lookup-test/iplookup.cs
+++ test/managed/lookup-test/iplookup.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.Contracts;
+using System.IO;
+using System.Net;
 
 namespace E2D2.Collections {
   class IPLookup {
@@ -12,7 +14,7 @@ namespace E2D2.Collections {
     private uint currentTBLLong_;
     const uint TBL24_SIZE = (1u << 24) + 1;
     const uint TBLLONG_SIZE = (1u << 24) + 1;
-    const UInt16 OVERFLOW_MASK = 0x8000u;
+    const UInt16 OVERFLOW_MASK = 0x8000;
 
     public IPLookup() {
       tbl24_ = new UInt16[TBL24_SIZE];
@@ -24,7 +26,7 @@ namespace E2D2.Collections {
       }
     }
 
-    public void AddRoute(UInt32 address, UInt16 length; UInt16 nextHop) {
+    public void AddRoute(UInt32 address, UInt16 length, UInt16 nextHop) {
       prefixTable_[length][address] = nextHop;
     }
 
@@ -34,7 +36,7 @@ namespace E2D2.Collections {
 
     public void ConstructFIB() {
       // Order gives us LPM.
-      for (uint i = 0; i <= 24; i++) {
+      for (int i = 0; i <= 24; i++) {
         foreach(var it in prefixTable_[i]) {
           UInt32 address = it.Key;
           UInt16 dest = it.Value;
@@ -48,13 +50,13 @@ namespace E2D2.Collections {
         }
       }
       // For these ones we want to fill up the overflow table too.
-      for (uint i = 25; i <= 32; i++) {
+      for (int i = 25; i <= 32; i++) {
         foreach(var it in prefixTable_[i]) {
           UInt32 addr = it.Key;
           UInt16 dest = it.Value;
-          UInt32 tblDest = tbl24_[addr >> 8]; // Check what the main table currently has.
+          UInt16 tblDest = tbl24_[addr >> 8]; // Check what the main table currently has.
           // See if we have already spilled over for these 24 bits.
-          if (tblDest & OVERFLOW_MASK) {
+          if ((tblDest & OVERFLOW_MASK) == 0) {
             // We assign 256 entries for each overflow 24-bit prefix
             UInt32 start = currentTBLLong_ + (addr & 0xff); // The last 8-bits of the address.
             UInt32 end = start + (1u << (32 - i)); // How many of the 8-bit entries need to be filled.
@@ -64,7 +66,8 @@ namespace E2D2.Collections {
               } else {
                 tblLong_[j] = dest;
               }
-              tbl24_[addr >> 8] = (currentTBLLong_ >> 8) | OVERFLOW_MASK;
+              tbl24_[addr >> 8] = 
+                  (UInt16)((currentTBLLong_ >> 8) | OVERFLOW_MASK);
             }
           } else {
               UInt32 start = (((UInt32)(tblDest & (~OVERFLOW_MASK)) << 8) + (addr & 0xff));
@@ -79,7 +82,7 @@ namespace E2D2.Collections {
     
     public UInt16 RouteLookup(UInt32 ip) {
       UInt16 tblDest = tbl24_[ip >> 8];
-      if (tblDest & OVERFLOW_MASK) {
+      if ((tblDest & OVERFLOW_MASK) > 0) {
         int index = (int)(((UInt32)(tblDest & (~OVERFLOW_MASK)) << 8) + (ip & 0xff));
         return tblLong_[index];
       } else {
@@ -89,7 +92,35 @@ namespace E2D2.Collections {
   }
   public class ThroughputTest {
     static void Main(string[] args) {
+      if(args.Length < 2) {
+        Console.WriteLine("Usage: IPLookup <rib> <trace>");
+        return;
+      }
+      List<UInt32> trace = new List<UInt32>();
       IPLookup lookup = new IPLookup();
+      StreamReader ribReader = new StreamReader(args[0]);
+      while (ribReader.Peek() >= 0) {
+        String line = ribReader.ReadLine();
+        String[] parts = line.Split(' ');
+        String[] addrParts = parts[0].Split('/');
+        UInt16 dest = Convert.ToUInt16(parts[1]);
+        UInt16 len = Convert.ToUInt16(addrParts[1]);
+        IPAddress addr = IPAddress.Parse(addrParts[0]);
+        UInt32 addrAsInt = 
+            (UInt32)IPAddress.NetworkToHostOrder(
+                    BitConverter.ToInt32(
+                        addr.GetAddressBytes(), 0));
+        lookup.AddRoute(addrAsInt, len, dest);
+      }
+      lookup.ConstructFIB();
+      ribReader.Close();
+      StreamReader traceReader = new StreamReader(args[1]);
+      while (traceReader.Peek() >= 0) {
+          UInt32 address = Convert.ToUInt32(traceReader.ReadLine());
+          trace.Add(address);
+          Console.WriteLine("{0} {1}", address, 
+                  lookup.RouteLookup(address));
+      }
     }
   }
 }
