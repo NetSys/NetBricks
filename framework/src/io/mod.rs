0c6ae573aadfe1bfef46b09a4a6106a6ec700acf
Aurojit Panda <apanda@cs.berkeley.edu>
2016-03-22 11:00:07 -0700

    packet_batch is no longer publicly visible

--- framework/src/io/mod.rs
+++ framework/src/io/mod.rs
@@ -1,11 +1,9 @@
 pub use self::interface::*;
-pub use self::packet_batch::PacketBatch;
 pub use self::packet_batch::ParsedBatch;
 pub use self::packet_batch::TransformBatch;
 pub use self::packet_batch::ReplaceBatch;
 pub use self::packet_batch::ReceiveBatch;
 pub use self::packet_batch::SendBatch;
-// pub use self::packet_batch::Act;
 pub use self::packet_batch::Batch;
 pub use self::pmd::*;
 mod interface;
