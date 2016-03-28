185d23b68b6c78e9f372fc764087036f9350381b
Aurojit Panda <apanda@cs.berkeley.edu>
2016-03-28 11:44:36 -0700

    Partway to making build work

--- build.sh
+++ build.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
+# Stop on any errors
 set -e
-3rdparty/get-dpdk.sh
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+
+# Build DPDK
+$BASE_DIR/3rdparty/get-dpdk.sh
 proc="$(nproc)"
 make -j $proc -C native
-pushd framework
+
+# Build cargo
+pushd $BASE_DIR/cargo
 cargo build --release
+popd
+
+pushd $BASE_DIR/framework
+$BASE_DIR/cargo/target/release/cargo build --release
+popd
+
+pushd $BASE_DIR/test/framework-test
+$BASE_DIR/cargo/target/release/cargo build --release
 popd
