9e1c8a0750116c7fb27b85987c6181624b5bf47f
Aurojit Panda <apanda@cs.berkeley.edu>
2016-04-19 20:58:54 -0700

    Eliminate some printouts

--- test/delay-test/src/main.rs
+++ test/delay-test/src/main.rs
@@ -190,23 +190,22 @@ fn main() {
     let mut pkts_so_far = (0, 0);
     let mut last_printed = 0.;
     const MAX_PRINT_INTERVAL: f64 = 15.;
+    const PRINT_DELAY: f64 = 10.;
+    let sleep_delay = (PRINT_DELAY / 2.) as u64;
     let mut start = time::precise_time_ns() as f64 / CONVERSION_FACTOR;
-    let sleep_time = Duration::from_millis(5000);
+    let sleep_time = Duration::from_millis(sleep_delay);
     println!("0 OVERALL RX 0.00 TX 0.00 CYCLE_PER_DELAY 0 0 0");
     loop {
         thread::sleep(sleep_time); // Sleep for a bit
         let now = time::precise_time_ns() as f64 / CONVERSION_FACTOR;
-        if now - start > 5.0 {
+        if now - start > PRINT_DELAY {
             let mut rx = 0;
             let mut tx = 0;
             for port in &ports {
-                let mac = port.mac_address();
                 for q in 0..port.rxqs() {
                     let (rp, tp) = port.stats(q);
                     rx += rp;
                     tx += tp;
-                    println!("{:.2} PORT {} Q {} RX {} TX {}",
-                                   now, mac, q, tx, tx);
                 }
             }
             let pkts = (rx, tx);
