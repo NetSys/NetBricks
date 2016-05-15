27d01d3f757cad2f0cf33630a1e30df5fa7a52f0
Aurojit Panda <apanda@cs.berkeley.edu>
2016-05-15 11:26:15 -0700

    Move cargo to tools

--- build.sh
+++ build.sh
@@ -2,8 +2,13 @@
 # Stop on any errors
 set -e
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
-CARGO="$BASE_DIR/cargo/target/release/cargo"
+EXT_BASE="$BASE_DIR/3rdparty"
+TOOLS_BASE="$BASE_DIR/3rdparty/tools"
+CARGO="${TOOLS_BASE}/cargo/target/release/cargo"
 DPDK="$BASE_DIR/3rdparty/dpdk/build/lib/libdpdk.a"
+MUSL_TEST="${TOOLS_BASE}/lib/libc.a"
+RUST_TEST="${TOOLS_BASE}/bin/rustc"
+mkdir -p TOOLS_BASE
 
 deps () {
 	# Build DPDK
@@ -17,6 +22,18 @@ deps () {
 	else
 		echo "Cargo found, not building"
 	fi
+
+	if [ ! -e MUSL_TEST ]; then
+		musl
+	else
+		echo "Musl found, not building"
+	fi
+}
+
+clean_deps() {
+	rm $BASE_DIR/3rdparty/dpdk.tar.gz || true
+	rm -rf $BASE_DIR/3rdparty/dpdk || true
+	make -C $BASE_DIR/native clean || true
 }
 
 dpdk () {
@@ -24,10 +41,26 @@ dpdk () {
 	proc="$(nproc)"
 	make -j $proc -C $BASE_DIR/native
 }
+
 cargo () {
 	# Build cargo
 	pushd $BASE_DIR/cargo
 	cargo build --release
+	popd
+}
+
+musl () {
+	MUSL_DOWNLOAD_PATH="${EXT_BASE}/musl.tar.gz"
+	MUSL_RESULT="${EXT_BASE}/musl"
+	curl http://www.musl-libc.org/releases/musl-1.1.10.tar.gz \
+		-o "${MUSL_DOWNLOAD_PATH}"
+	mkdir -p ${MUSL_RESULT}
+	tar zxvf "${MUSL_DOWNLOAD_PATH}" \
+		-C "${MUSL_RESULT}" --strip-components=1
+	pushd ${MUSL_RESULT}
+	./configure --disable-shared --prefix="$TOOLS_BASE"
+	make -j
+	make install
 	popd
 }
 
@@ -115,10 +148,10 @@ case $TASK in
 		$BASE_DIR/cargo/target/release/cargo clean; $BASE_DIR/cargo/target/release/cargo build --features dev
 		popd
 		;;
+	dist_clean) 
+		clean_deps
+		;;
 	clean)
-		rm $BASE_DIR/3rdparty/dpdk.tar.gz || true
-		rm -rf $BASE_DIR/3rdparty/dpdk || true
-		make -C $BASE_DIR/native clean || true
 		pushd $BASE_DIR/framework
 		$BASE_DIR/cargo/target/release/cargo clean || true
 		popd
