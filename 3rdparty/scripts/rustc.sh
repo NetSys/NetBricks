db4eed9cfbc6a8d95af24fdb4d590bf0fc259648
Aurojit Panda <apanda@cs.berkeley.edu>
2016-09-10 10:54:28 -0700

    Remove printout

--- 3rdparty/scripts/rustc.sh
+++ 3rdparty/scripts/rustc.sh
@@ -4,6 +4,6 @@ BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 if [ -e $BASE_DIR/rustc ]; then
 	$BASE_DIR/rustc "$@"
 else
-    (>&2 echo "WARNING: Using system rustc")
+    #(>&2 echo "WARNING: Using system rustc")
 	rustc "$@"
 fi
