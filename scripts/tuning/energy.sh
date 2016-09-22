4f8a9fa31e8a74bd2eec9f7dd8f1eea02b1aa17f
Aurojit Panda <apanda@cs.berkeley.edu>
2016-09-22 09:55:18 -0700

    Move tuning scripts into scripts

--- scripts/tuning/energy.sh
+++ scripts/tuning/energy.sh
@@ -2,11 +2,10 @@
 set -x
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 sudo modprobe msr
-# Debian does not seem to install x86_energy_perf_policy :(
 if [ -e ${BASE_DIR}/x86_energy_perf_policy ]; then
-    sudo $BASE_DIR/x86_energy_perf_policy performance
+    sudo $BASE_DIR/x86_energy_perf_policy performance # Set ourselves to performance.
 else
     sudo x86_energy_perf_policy performance
 fi
-sudo $BASE_DIR/pmqos-static.py cpu_dma_latency=0
+sudo $BASE_DIR/pmqos-static.py cpu_dma_latency=0 # Tune Linux QoS to reduce DMA latency
 sudo wrmsr -a 0x620 0x3f3f # Turn off uncore frequency scaling and select max frequency
