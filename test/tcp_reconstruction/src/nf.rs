6502d8bb4c9a4bebb113049f9e4f8a62d923ebb9
Aurojit Panda <apanda@cs.berkeley.edu>
2016-10-06 00:29:08 -0700

    NF with working reconstruction
    The main problem now is limiting memory usage

--- test/tcp_reconstruction/src/nf.rs
+++ test/tcp_reconstruction/src/nf.rs
@@ -1,29 +1,81 @@
 use e2d2::utils::Flow;
 use e2d2::headers::*;
+use e2d2::scheduler::*;
 use e2d2::operators::*;
+use e2d2::state::*;
+use std::str;
 use std::collections::HashMap;
 use fnv::FnvHasher;
 use std::hash::BuildHasherDefault;
 
 type FnvHash = BuildHasherDefault<FnvHasher>;
+const BUFFER_SIZE: usize = 10240; // 20K of buffers
+const PRINT_SIZE: usize = 256;
 
-pub fn reconstruction<T: 'static + Batch<Header = NullHeader>>(parent: T) -> CompositionBatch {
-    let mut cache = HashMap::<Flow, usize, FnvHash>::with_hasher(Default::default());
-    parent.parse::<MacHeader>()
+pub fn reconstruction<T: 'static + Batch<Header = NullHeader>>(parent: T, sched: &mut Scheduler) -> CompositionBatch {
+    let mut cache = HashMap::<Flow, ReorderedBuffer, FnvHash>::with_hasher(Default::default());
+    let mut read_buf: Vec<u8> = (0..PRINT_SIZE).map(|_| 0).collect();
+    let mut groups = parent.parse::<MacHeader>()
         .transform(box move |p| {
             p.get_mut_header().swap_addresses();
         })
         .parse::<IpHeader>()
-        .filter(box move |p| p.get_header().protocol() == 6)
+        .group_by(2,
+                  box move |p| {
+            if p.get_header().protocol() == 6 {
+                0
+            } else {
+                1
+            }
+        },
+                  sched);
+
+    let pipe = groups.get_group(0)
+        .unwrap()
         .metadata(box move |p| {
             let flow = p.get_header().flow().unwrap();
             flow
         })
         .parse::<TcpHeader>()
         .transform(box move |p| {
-            let flow = p.read_metadata();
-            let mut e = cache.entry(*flow).or_insert_with(|| 0);
-            *e = *e + 1;
+            if !p.get_header().psh_flag() {
+                let flow = p.read_metadata();
+                let mut e = cache.entry(*flow).or_insert_with(|| ReorderedBuffer::new(BUFFER_SIZE));
+                let seq = p.get_header().seq_num();
+                let result = if p.get_header().syn_flag() {
+                    e.seq(seq, p.get_payload())
+                } else if e.is_established() {
+                    e.add_data(seq, p.get_payload())
+                } else {
+                    e.seq(seq, p.get_payload())
+                };
+                match result {
+                    InsertionResult::Inserted { available, .. } => {
+                        if available > PRINT_SIZE {
+                            let mut read = 0;
+                            while available - read > PRINT_SIZE {
+                                let avail = e.read_data(&mut read_buf[0..]);
+                                read += avail;
+                                match str::from_utf8(&read_buf[0..avail]) {
+                                    Ok(_s) => {
+                                        // println!("Read {}", s);
+                                    }
+                                    _ => (),
+                                }
+                                println!("Read {}", str::from_utf8(&read_buf[0..avail]).unwrap());
+                            }
+                        }
+                    }
+                    InsertionResult::OutOfMemory { .. } => {
+                        // InsertionResult::OutOfMemory { written, available } => {
+                        // println!("{}", p.get_header());
+                    }
+                };
+                if p.get_header().rst_flag() {
+                    e.reset();
+                }
+            }
         })
-        .compose()
+        .compose();
+    merge(vec![pipe, groups.get_group(1).unwrap().compose()]).compose()
 }
