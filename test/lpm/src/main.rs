2ba5855b71c8b8b0de5bd9d63fd88df54170d523
Aurojit Panda <aurojit@gmail.com>
2017-01-18 15:21:43 -0800

    Fixes and overflow in LPM (#40)
    * Commiting to pull
    
    * lmp redone, seems to be working
    
    * Fixes an overflow error in LPM
    
    This subsumes pull request #38 because I was evil.
    
    * Fix LPM lookup
    
    I misunderstood operator precedence.

--- test/lpm/src/main.rs
+++ test/lpm/src/main.rs
@@ -4,15 +4,16 @@ extern crate fnv;
 extern crate time;
 extern crate getopts;
 extern crate rand;
-use e2d2::allocators::CacheAligned;
+use e2d2::common::*;
+use e2d2::config::*;
 use e2d2::interface::*;
-use e2d2::interface::dpdk::*;
 use e2d2::operators::*;
 use e2d2::scheduler::*;
 use getopts::Options;
 use self::nf::*;
 use std::collections::HashMap;
 use std::env;
+use std::fmt::Display;
 use std::process;
 use std::sync::Arc;
 use std::thread;
@@ -21,28 +22,22 @@ mod nf;
 
 const CONVERSION_FACTOR: f64 = 1000000000.;
 
-fn recv_thread(ports: Vec<CacheAligned<PortQueue>>, core: i32) {
-    init_thread(core, core);
-    let mut sched = StandaloneScheduler::new();
+fn test<T, S>(ports: Vec<T>, sched: &mut S)
+    where T: PacketRx + PacketTx + Display + Clone + 'static,
+          S: Scheduler + Sized
+{
     println!("Receiving started");
     for port in &ports {
-        println!("Receiving port {} rxq {} txq {} on core {}",
-                 port.port.mac_address(),
-                 port.rxq(),
-                 port.txq(),
-                 core);
+        println!("Receiving port {}", port);
     }
 
-    let mut pipelines: Vec<_> = ports.iter()
-        .map(|port| lpm(ReceiveBatch::new(port.clone()), &mut sched).send(port.clone()))
+    let pipelines: Vec<_> = ports.iter()
+        .map(|port| lpm(ReceiveBatch::new(port.clone()), sched).send(port.clone()))
         .collect();
     println!("Running {} pipelines", pipelines.len());
-    if pipelines.len() > 1 {
-        sched.add_task(merge(pipelines)).unwrap()
-    } else {
-        sched.add_task(pipelines.pop().unwrap()).unwrap()
-    };
-    sched.execute_loop();
+    for pipeline in pipelines {
+        sched.add_task(pipeline).unwrap();
+    }
 }
 
 fn main() {
@@ -51,10 +46,13 @@ fn main() {
     let mut opts = Options::new();
     opts.optflag("h", "help", "print this help menu");
     opts.optflag("", "secondary", "run as a secondary process");
+    opts.optflag("", "primary", "run as a primary process");
     opts.optopt("n", "name", "name to use for the current process", "name");
     opts.optmulti("p", "port", "Port to use", "[type:]id");
     opts.optmulti("c", "core", "Core to use", "core");
     opts.optopt("m", "master", "Master core", "master");
+    opts.optopt("f", "configuration", "Configuration file", "path");
+    opts.optflag("t", "test", "Test mode do not use real ports");
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => m,
         Err(f) => panic!(f.to_string()),
@@ -64,18 +62,48 @@ fn main() {
         process::exit(0)
     }
 
-    let cores_str = matches.opt_strs("c");
-    let master_core = matches.opt_str("m")
-        .unwrap_or_else(|| String::from("0"))
-        .parse()
-        .expect("Could not parse master core spec");
-    println!("Using master core {}", master_core);
-    let name = matches.opt_str("n").unwrap_or_else(|| String::from("recv"));
+    let configuration = if matches.opt_present("f") {
+        let config_file = matches.opt_str("f").unwrap();
+        match read_configuration(&config_file[..]) {
+            Ok(cfg) => cfg,
+            Err(ref e) => {
+                print_error(e);
+                process::exit(1);
+            }
+        }
+    } else {
+        let name = matches.opt_str("n").unwrap_or_else(|| String::from("recv"));
+        NetbricksConfiguration::new_with_name(&name[..])
+    };
 
-    let cores: Vec<i32> = cores_str.iter()
-        .map(|n: &String| n.parse().ok().expect(&format!("Core cannot be parsed {}", n)))
-        .collect();
 
+    let configuration = if matches.opt_present("m") {
+        NetbricksConfiguration {
+            primary_core: matches.opt_str("m")
+                .unwrap()
+                .parse()
+                .expect("Could not parse master core"),
+            strict: true,
+            ..configuration
+        }
+    } else {
+        configuration
+    };
+
+
+    let configuration = if matches.opt_present("secondary") {
+        NetbricksConfiguration { secondary: true, ..configuration }
+    } else {
+        configuration
+    };
+
+    let configuration = if matches.opt_present("primary") {
+        NetbricksConfiguration { secondary: false, ..configuration }
+    } else {
+        configuration
+    };
+
+    let phy_ports = !matches.opt_present("test");
 
     fn extract_cores_for_port(ports: &[String], cores: &[i32]) -> HashMap<String, Vec<i32>> {
         let mut cores_for_port = HashMap::<String, Vec<i32>>::new();
@@ -85,75 +113,98 @@ fn main() {
         cores_for_port
     }
 
-    let primary = !matches.opt_present("secondary");
 
-    let cores_for_port = extract_cores_for_port(&matches.opt_strs("p"), &cores);
+    let configuration = if matches.opt_present("c") {
 
-    if primary {
-        init_system_wl(&name, master_core, &[]);
-    } else {
-        init_system_secondary(&name, master_core);
-    }
+        let cores_str = matches.opt_strs("c");
 
-    let ports_to_activate: Vec<_> = cores_for_port.keys().collect();
+        let mut cores: Vec<i32> = cores_str.iter()
+            .map(|n: &String| n.parse().ok().expect(&format!("Core cannot be parsed {}", n)))
+            .collect();
 
-    let mut queues_by_core = HashMap::<i32, Vec<_>>::with_capacity(cores.len());
-    let mut ports = Vec::<Arc<PmdPort>>::with_capacity(ports_to_activate.len());
-    for port in &ports_to_activate {
-        let cores = cores_for_port.get(*port).unwrap();
-        let queues = cores.len() as i32;
-        let pmd_port = PmdPort::new_with_queues(*port, queues, queues, cores, cores)
-            .expect("Could not initialize port");
-        for (idx, core) in cores.iter().enumerate() {
-            let queue = idx as i32;
-            queues_by_core.entry(*core)
-                .or_insert(vec![])
-                .push(PmdPort::new_queue_pair(&pmd_port, queue, queue).unwrap());
+
+        let cores_for_port = extract_cores_for_port(&matches.opt_strs("p"), &cores);
+
+        let ports_to_activate: Vec<_> = cores_for_port.keys().collect();
+
+        let mut ports = Vec::with_capacity(ports_to_activate.len());
+
+        for port in &ports_to_activate {
+            let cores = cores_for_port.get(*port).unwrap();
+            ports.push(PortConfiguration::new_with_queues(*port, cores, cores))
         }
-        ports.push(pmd_port);
-    }
+        cores.dedup();
+        NetbricksConfiguration {
+            cores: cores,
+            ports: ports,
+            ..configuration
+        }
+    } else {
+        configuration
+    };
 
-    const _BATCH: usize = 1 << 10;
-    const _CHANNEL_SIZE: usize = 256;
-    let _thread: Vec<_> = queues_by_core.iter()
-        .map(|(core, ports)| {
-            let c = core.clone();
-            let p: Vec<_> = ports.iter().map(|p| p.clone()).collect();
-            std::thread::spawn(move || recv_thread(p, c))
-        })
-        .collect();
-    let mut pkts_so_far = (0, 0);
-    let mut last_printed = 0.;
-    const MAX_PRINT_INTERVAL: f64 = 10.;
-    const PRINT_DELAY: f64 = 5.;
-    let sleep_delay = (PRINT_DELAY / 2.) as u64;
-    let mut start = time::precise_time_ns() as f64 / CONVERSION_FACTOR;
-    let sleep_time = Duration::from_millis(sleep_delay);
-    println!("0 OVERALL RX 0.00 TX 0.00 CYCLE_PER_DELAY 0 0 0");
-    loop {
-        thread::sleep(sleep_time); // Sleep for a bit
-        let now = time::precise_time_ns() as f64 / CONVERSION_FACTOR;
-        if now - start > PRINT_DELAY {
-            let mut rx = 0;
-            let mut tx = 0;
-            for port in &ports {
-                for q in 0..port.rxqs() {
-                    let (rp, tp) = port.stats(q);
-                    rx += rp;
-                    tx += tp;
+    println!("Going to start with configuration {}", configuration);
+
+    match initialize_system(&configuration) {
+        Ok(mut context) => {
+            context.start_schedulers();
+
+            if phy_ports {
+                context.add_pipeline_to_run(Arc::new(move |p, s: &mut StandaloneScheduler| test(p, s)));
+            } else {
+                context.add_test_pipeline(Arc::new(move |p, s: &mut StandaloneScheduler| test(p, s)));
+            }
+            context.execute();
+
+            let mut pkts_so_far = (0, 0);
+            let mut last_printed = 0.;
+            const MAX_PRINT_INTERVAL: f64 = 30.;
+            const PRINT_DELAY: f64 = 15.;
+            let sleep_delay = (PRINT_DELAY / 2.) as u64;
+            let mut start = time::precise_time_ns() as f64 / CONVERSION_FACTOR;
+            let sleep_time = Duration::from_millis(sleep_delay);
+            println!("0 OVERALL RX 0.00 TX 0.00 CYCLE_PER_DELAY 0 0 0");
+            loop {
+                thread::sleep(sleep_time); // Sleep for a bit
+                let now = time::precise_time_ns() as f64 / CONVERSION_FACTOR;
+                if now - start > PRINT_DELAY {
+                    let mut rx = 0;
+                    let mut tx = 0;
+                    if phy_ports {
+                        for port in context.ports.values() {
+                            for q in 0..port.rxqs() {
+                                let (rp, tp) = port.stats(q);
+                                rx += rp;
+                                tx += tp;
+                            }
+                        }
+                    } else {
+                        for port in context.virtual_ports.values() {
+                            let (rp, tp) = port.stats();
+                            rx += rp;
+                            tx += tp;
+                        }
+                    }
+                    let pkts = (rx, tx);
+                    let rx_pkts = pkts.0 - pkts_so_far.0;
+                    if rx_pkts > 0 || now - last_printed > MAX_PRINT_INTERVAL {
+                        println!("{:.2} OVERALL RX {:.2} TX {:.2}",
+                                 now - start,
+                                 rx_pkts as f64 / (now - start),
+                                 (pkts.1 - pkts_so_far.1) as f64 / (now - start));
+                        last_printed = now;
+                        start = now;
+                        pkts_so_far = pkts;
+                    }
                 }
             }
-            let pkts = (rx, tx);
-            let rx_pkts = pkts.0 - pkts_so_far.0;
-            if rx_pkts > 0 || now - last_printed > MAX_PRINT_INTERVAL {
-                println!("{:.2} OVERALL RX {:.2} TX {:.2}",
-                         now - start,
-                         rx_pkts as f64 / (now - start),
-                         (pkts.1 - pkts_so_far.1) as f64 / (now - start));
-                last_printed = now;
-                start = now;
-                pkts_so_far = pkts;
+        }
+        Err(ref e) => {
+            println!("Error: {}", e);
+            if let Some(backtrace) = e.backtrace() {
+                println!("Backtrace: {:?}", backtrace);
             }
+            process::exit(1);
         }
     }
 }
--- test/lpm/src/nf.rs
+++ test/lpm/src/nf.rs
@@ -75,7 +75,7 @@ impl IPLookup {
                     self.tbl24[addr >> 8] = ((ctlb >> 8) as u16) | OVERFLOW_MASK;
                     self.current_tbl_long += 256;
                 } else {
-                    let start = ((t24entry & (!OVERFLOW_MASK)) as usize) << 8 + (addr & 0xff);
+                    let start = (((t24entry & (!OVERFLOW_MASK)) as usize) << 8) + (addr & 0xff);
                     let end = start + (1 << (32 - i));
                     for j in start..end {
                         self.tbl_long[j] = *v;
@@ -90,7 +90,7 @@ impl IPLookup {
         let addr = ip as usize;
         let t24entry = self.tbl24[addr >> 8];
         if (t24entry & OVERFLOW_MASK) > 0 {
-            let index = ((t24entry & !OVERFLOW_MASK) as usize) << 8 + (addr & 0xff);
+            let index = (((t24entry & !OVERFLOW_MASK) as usize) << 8) + (addr & 0xff);
             self.tbl_long[index]
         } else {
             t24entry
@@ -103,9 +103,111 @@ pub fn lpm<T: 'static + Batch<Header = NullHeader, Metadata = EmptyMetadata>, S:
      s: &mut S)
      -> CompositionBatch {
     let mut lpm_table = IPLookup::new();
-    lpm_table.insert_ipv4(&Ipv4Addr::new(0, 0, 0, 0), 0, 1);
-    lpm_table.insert_ipv4(&Ipv4Addr::new(10, 0, 0, 0), 8, 2);
-    lpm_table.insert_ipv4(&Ipv4Addr::new(192, 0, 0, 0), 8, 2);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(188, 19, 50, 135), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(123, 19, 205, 58), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(58, 218, 199, 165), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(61, 90, 38, 155), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(190, 179, 91, 29), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(198, 23, 250, 66), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(42, 103, 111, 67), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(117, 197, 187, 144), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(207, 198, 106, 183), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(122, 90, 22, 43), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(5, 167, 64, 38), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(152, 166, 114, 31), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(191, 81, 59, 58), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(190, 175, 182, 182), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(190, 237, 89, 70), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(181, 21, 43, 134), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(122, 171, 197, 247), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(113, 212, 69, 195), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(151, 74, 149, 41), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(27, 28, 145, 139), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(206, 169, 145, 35), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(105, 103, 122, 43), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(116, 45, 32, 133), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(202, 109, 166, 177), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(195, 53, 118, 92), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(5, 167, 65, 10), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(216, 151, 137, 247), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(103, 56, 182, 207), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(216, 151, 138, 92), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(88, 250, 194, 52), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(41, 232, 211, 169), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(106, 215, 170, 250), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(187, 3, 143, 162), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(110, 85, 81, 79), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(220, 111, 212, 68), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(185, 151, 210, 156), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(190, 179, 180, 225), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(2, 92, 39, 245), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(107, 191, 202, 53), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(46, 161, 9, 22), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(122, 117, 64, 197), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(186, 133, 153, 160), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(36, 255, 211, 72), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(173, 234, 225, 96), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(95, 68, 215, 116), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(115, 84, 82, 217), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(84, 164, 153, 138), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(190, 50, 251, 94), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(93, 105, 249, 221), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(37, 21, 101, 156), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(143, 0, 222, 235), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(5, 167, 69, 45), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(37, 191, 159, 133), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(203, 134, 213, 65), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(84, 211, 75, 56), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(117, 248, 162, 119), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(69, 178, 195, 20), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(113, 189, 147, 99), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(185, 84, 202, 52), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(188, 143, 232, 190), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(41, 221, 50, 102), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(92, 82, 174, 27), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(113, 55, 12, 76), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(62, 211, 191, 202), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(191, 82, 178, 107), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(181, 23, 198, 10), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(117, 196, 200, 35), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(117, 198, 60, 179), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(178, 136, 73, 133), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(203, 130, 228, 60), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(179, 41, 211, 34), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(193, 92, 162, 10), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(216, 151, 130, 236), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(2, 185, 182, 1), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(185, 148, 100, 45), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(85, 105, 157, 175), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(171, 80, 158, 237), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(109, 165, 67, 16), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(5, 167, 66, 53), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(183, 165, 159, 231), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(152, 232, 193, 27), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(60, 184, 112, 190), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(122, 163, 104, 12), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(177, 213, 230, 190), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(113, 146, 90, 33), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(190, 49, 119, 180), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(89, 187, 144, 19), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(31, 168, 81, 84), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(201, 254, 169, 36), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(200, 71, 53, 112), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(90, 189, 133, 179), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(24, 219, 68, 17), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(190, 178, 143, 49), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(41, 105, 236, 253), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(183, 60, 48, 25), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(188, 143, 232, 254), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(36, 97, 169, 81), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(192, 99, 147, 251), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(171, 233, 174, 141), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(112, 227, 158, 252), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(188, 143, 233, 59), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(192, 209, 125, 92), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(5, 167, 66, 37), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(95, 215, 103, 88), 32, 1);
+    lpm_table.insert_ipv4(&Ipv4Addr::new(5, 167, 65, 50), 32, 1);
     lpm_table.construct_table();
     let mut groups = parent.parse::<MacHeader>()
         .transform(box |p| p.get_mut_header().swap_addresses())
