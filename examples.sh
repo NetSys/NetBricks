1a41c6de316b9728fd8328b549e94330340eac43
Steven H. Wang <wang.steven.h@gmail.com>
2017-05-26 17:31:02 +0000

    integration tests: Make names consistent
    With this commit, tests are named the same as their build directories.
    Tests names no longer begin with 'zcsi'.
    
    Consistent naming simplifies the task of compiling debug binaries for
    the `./build.sh debug` command in a later commit.

--- examples.sh
+++ examples.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 # Lists all the examples in Bess. This is used by the build script.
 export examples=(
-        test/framework-test
-        test/delay-test
-        test/shutdown-test
-        test/chain-test
+        test/acl-fw
+        test/chain
+        test/config
+        test/delay
+        test/embedded-sched
+        test/embedded-sched-dep
+        test/framework
         test/lpm
         test/lpm-embedded
-        test/nat
         test/maglev
-        test/tcp_check
-        test/sctp-test
-        test/config-test
+        test/nat
+        test/packet
+        test/packet-gen
         test/reset-parse
-        test/tcp_reconstruction
-        test/acl-fw
-        test/packet_generation
-        test/packet_test
-        test/embedded-scheduler-test
-        test/embedded-scheduler-dependency-test
+        test/sctp
+        test/shutdown
+        test/tcp
+        test/tcp-reconstruct
 )
 
