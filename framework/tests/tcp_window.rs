b562e4b7c33728e5a457414bec04610a1d39b112
ncarter100 <ncarter100@gmail.com>
2017-06-05 17:32:53 +0100

    test tcp window creation_test fails, cannot alloc > 1024MB (#62)
    dpdk requires 1024MB Hugepages to run. But when the host
    has allocated exactly that amount, tcp_window.rs creation_test
    fails as when i == 1073741824, ReorderedBuffer::new(i + 1) asks
    for more bytes than we have in Hugepages.
    
    Change so the max i is /2. new(i + 1) will still test an
    allocation requiring all pages, since the pages are always a
    power of 2.

--- framework/tests/tcp_window.rs
+++ framework/tests/tcp_window.rs
@@ -58,11 +58,13 @@ fn round_to_power_of_2_test() {
 #[test]
 fn creation_test() {
     let mut i = 32;
-    while i <= 1073741824 {
+    while i <= 1073741824 / 2 {
+        // /2 so test passes on host with 1024MB Hugepages
         let ro = ReorderedBuffer::new(i).unwrap();
         assert_eq!(i, ro.buffer_size());
         drop(ro);
-        let ro = ReorderedBuffer::new(i + 1).unwrap();
+
+        let ro = ReorderedBuffer::new(i + 1).unwrap(); // alloc next power of 2
         assert_eq!(i * 2, ro.buffer_size());
         drop(ro);
         i *= 2;
