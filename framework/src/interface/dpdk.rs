180ff18929fac038b3a2e1cf4e96f58aa21b2edf
Aurojit Panda <apanda@cs.berkeley.edu>
2017-12-29 15:07:36 -0800

    Eliminate more things that call into libzcsi

--- framework/src/interface/dpdk.rs
+++ framework/src/interface/dpdk.rs
@@ -1,21 +1,46 @@
 use super::METADATA_SLOTS;
 use super::native_include as ldpdk;
-use config::{DEFAULT_CACHE_SIZE, DEFAULT_POOL_SIZE, NetbricksConfiguration};
+use config::{NetbricksConfiguration, DEFAULT_CACHE_SIZE, DEFAULT_POOL_SIZE};
 use native::libnuma;
 use native::zcsi;
 use std::cell::Cell;
+use std::cmp;
 use std::ffi::CString;
-use std::ptr;
 use std::iter;
+use std::ptr;
+
+unsafe fn init_socket_mempool(
+    socket: i32,
+    pool_size: u32,
+    cache_size: u32,
+    metadata_slots: u16,
+) -> *mut ldpdk::rte_mempool {
+    let name = CString::new(format!("pframe{}", socket)).unwrap();
+    ldpdk::rte_pktmbuf_pool_create(
+        name.into_raw(),
+        pool_size,
+        cache_size,
+        metadata_slots * 64,
+        ldpdk::RTE_MBUF_DEFAULT_BUF_SIZE as u16,
+        socket,
+    )
+}
+
+unsafe fn bind_thread_to_numa_node(socket: u32) {
+    let bitmask = libnuma::numa_bitmask_setbit(
+        libnuma::numa_bitmask_clearall(libnuma::numa_bitmask_alloc(
+            libnuma::numa_num_possible_nodes() as u32,
+        )),
+        socket,
+    );
+    libnuma::numa_bind(bitmask);
+}
 
 /// Initialize the system, whitelisting some set of NICs and allocating mempool of given size.
 fn init_system_wl_with_mempool(name: &str, core: i32, devices: &[String], pool_size: u32, cache_size: u32) {
     let name_cstr = CString::new(name).unwrap();
 
-    //let pci_cstr: Vec<_> = pci.iter().map(|p| CString::new(&p[..]).unwrap()).collect();
-    //let mut whitelist: Vec<_> = pci_cstr.iter().map(|p| p.as_ptr()).collect();
     let mut dpdk_args = vec![];
-    //let core_str = CString::new(format!("0x{:x}", 1u32 << core)).unwrap();
     // FIXME: Maybe replace with placement syntax
     // First we need to push in name.
     unsafe {
@@ -24,12 +49,23 @@ fn init_system_wl_with_mempool(name: &str, core: i32, devices: &[String], pool_s
         // Using RTE_MAX_LCORE as core ID for master.
         let master_lcore = ldpdk::RTE_MAX_LCORE - 1;
         dpdk_args.push(CString::new(master_lcore.to_string()).unwrap().into_raw());
-        dpdk_args.push(CString::new(format!("{}@{}", master_lcore, core)).unwrap().into_raw());
+        dpdk_args.push(
+            CString::new(format!("{}@{}", master_lcore, core))
+                .unwrap()
+                .into_raw(),
+        );
         dpdk_args.push(CString::new("--no-shconf").unwrap().into_raw());
 
-        // Fix this
-        let numa_nodes = 2;
-        let mem_vec : Vec<_> = iter::repeat(pool_size.to_string()).take(numa_nodes).collect();
+        let numa_nodes = if libnuma::numa_available() == -1 {
+            // FIXME: Warn.
+            1
+        } else {
+            // The cmp::max is to take care of any cases where libnuma is broken.
+            cmp::max(1, libnuma::numa_num_configured_nodes())
+        } as usize;
+        let mem_vec: Vec<_> = iter::repeat(pool_size.to_string())
+            .take(numa_nodes)
+            .collect();
         let mem = mem_vec.join(",");
         dpdk_args.push(CString::new("--socket-mem").unwrap().into_raw());
         dpdk_args.push(CString::new(mem).unwrap().into_raw());
@@ -47,32 +83,19 @@ fn init_system_wl_with_mempool(name: &str, core: i32, devices: &[String], pool_s
         if ret != 0 {
             panic!("Could not initialize DPDK -- errno {}", -ret)
         }
+
+        let socket = ldpdk::lcore_config[master_lcore as usize].socket_id;
+        bind_thread_to_numa_node(socket);
+        for sock in 0..numa_nodes {
+            let _ = init_socket_mempool(sock as i32, pool_size, cache_size, METADATA_SLOTS as u16);
+        }
+        set_lcore_id(master_lcore as i32);
     }
 }
 
 /// Initialize the system, whitelisting some set of NICs.
 pub fn init_system_wl(name: &str, core: i32, pci: &[String]) {
     init_system_wl_with_mempool(name, core, pci, DEFAULT_POOL_SIZE, DEFAULT_CACHE_SIZE);
-    set_numa_domain();
-}
-
-/// Initialize the system as a DPDK secondary process with a set of VDEVs. User must specify mempool name to use.
-pub fn init_system_secondary(name: &str, core: i32) {
-    let name_cstr = CString::new(name).unwrap();
-    let mut vdev_list = vec![];
-    unsafe {
-        let ret = zcsi::init_secondary(
-            name_cstr.as_ptr(),
-            name.len() as i32,
-            core,
-            vdev_list.as_mut_ptr(),
-            0,
-        );
-        if ret != 0 {
-            panic!("Could not initialize secondary process errno {}", ret)
-        }
-    }
-    set_numa_domain();
 }
 
 /// Initialize the system based on the supplied scheduler configuration.
@@ -81,51 +104,29 @@ pub fn init_system(config: &NetbricksConfiguration) {
         panic!("Configuration must provide a name.");
     }
     // We init with all devices blacklisted and rely on the attach logic to white list them as necessary.
-    if config.secondary {
-        // We do not have control over any of the other settings in this case.
-        init_system_secondary(&config.name[..], config.primary_core);
-    } else {
-        init_system_wl_with_mempool(
-            &config.name[..],
-            config.primary_core,
-            &[],
-            config.pool_size,
-            config.cache_size,
-        );
-    }
-    set_numa_domain();
+    init_system_wl_with_mempool(
+        &config.name[..],
+        config.primary_core,
+        &[],
+        config.pool_size,
+        config.cache_size,
+    );
 }
 
-thread_local!(static NUMA_DOMAIN: Cell<i32> = Cell::new(-1));
+thread_local!(static CURRENT_LCORE: Cell<i32> = Cell::new(-1));
 
-fn set_numa_domain() {
-    let domain = unsafe {
-        if libnuma::numa_available() == -1 {
-            println!("No NUMA information found, support disabled");
-            -1
-        } else {
-            let domain = libnuma::numa_preferred();
-            println!("Running on node {}", domain);
-            domain
-        }
-    };
-    NUMA_DOMAIN.with(|f| f.set(domain))
+fn set_lcore_id(lcore: i32) {
+    CURRENT_LCORE.with(|f| f.set(lcore))
 }
 
 /// Affinitize a pthread to a core and assign a DPDK thread ID.
 pub fn init_thread(tid: i32, core: i32) {
-    let numa = unsafe { zcsi::init_thread(tid, core) };
-    NUMA_DOMAIN.with(|f| {
-        f.set(numa);
-    });
-    if numa == -1 {
-        println!("No NUMA information found, support disabled");
-    } else {
-        println!("Running on node {}", numa);
-    };
+    unsafe { zcsi::init_thread(tid, core) };
+    set_lcore_id(tid)
 }
 
 #[inline]
-pub fn get_domain() -> i32 {
-    NUMA_DOMAIN.with(|f| f.get())
+pub fn get_domain() -> u32 {
+    let lcore = CURRENT_LCORE.with(|f| f.get());
+    unsafe { ldpdk::lcore_config[lcore as usize].socket_id }
 }
