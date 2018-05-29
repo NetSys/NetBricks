97024a8d03a6e0aa1fdfc591425ca87d2667de9a
Aurojit Panda <apanda@cs.berkeley.edu>
2018-05-29 13:59:29 -0700

    fixup! Change unused_doc_comments

--- framework/src/lib.rs
+++ framework/src/lib.rs
@@ -15,7 +15,7 @@
 #![feature(allocator_api)]
 #![allow(unused_features)]
 #![feature(integer_atomics)]
-#![allow(unused_doc_comment)]
+#![allow(unused_doc_comments)]
 #![cfg_attr(feature = "dev", allow(unstable_features))]
 // Need this since PMD port construction triggers too many arguments.
 #![cfg_attr(feature = "dev", allow(too_many_arguments))]
