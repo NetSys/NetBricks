219ecd5250a50ba6907121e89da54e8f3692ef11
crolli001c <Stephen_Rollins@comcast.com>
2018-09-02 13:43:08 -0400

    change message type from int to enum

--- framework/src/headers/icmp.rs
+++ framework/src/headers/icmp.rs
@@ -1,23 +1,34 @@
 use headers::ip::IpHeader;
 use std::default::Default;
 use std::fmt;
-
-pub struct IcmpV6Header<T> {
-    msg_type: u8,
-    code: u8,
-    checksum: u16
-}
+#[macro_use]
+extern crate enum_primitive;
+use enum_primitive::FromPrimitive;
 
 pub const ROUTER_ADVERTISEMENT: int = 134;
 pub const NEIGHBOR_SOLICITATION: int = 135;
 pub const NEIGHBOR_ADVERTISEMENT: int = 136;
+
+enum_from_primitive! {
+#[derive(Debug, PartialEq)]
+pub enum IcmpMessageType {
+    RouterAdvertisement = ROUTER_ADVERTISEMENT,
+    NeighborSolicitation = NEIGHBOR_SOLICITATION,
+    NeighborAdvertisement = NEIGHBOR_ADVERTISEMENT,
+}
+}
+
+pub struct IcmpV6Header<T> {
+    msg_type: IcmpMessageType,
+    code: u8,
+    checksum: u16
+}
 
 impl Default<T> for IcmpV6Header<T>
     where T: IpHeader
 {
     fn default() -> IcmpV6Header<T> {
         IcmpHeader {
-            msg_type: 0,
             code: 0,
             checksum: 0
         }
@@ -38,13 +49,22 @@ impl<T> IcmpV6Header<T>
     where T: IpHeader,
 {
     #[inline]
-    pub fn set_msg_type(&mut self, src: Ipv6Addr) {
-        self.src_ip = src
+    pub fn set_msg_type(&mut self, msg_type: IcmpMessageType) {
+        self.msg_type = msg_type;
     }
 
     #[inline]
-    pub fn set_code(&mut self, code: u8) { self.code = code}
-
-    #[inline]
     pub fn set_checksum(&mut self, checksum: u16) { self.checksum = checksum}
+}
+
+#[cfg(test)]
+mod tests  {
+
+    #[test]
+    fn test_icmp_message_types() {
+        assert_eq!(IcmpMessageType::from_i32(134), Some(IcmpMessageType::RouterAdvertisement));
+        assert_eq!(IcmpMessageType::from_i32(135), Some(IcmpMessageType::NeighborSolicitation));
+        assert_eq!(IcmpMessageType::from_i32(136), Some(IcmpMessageType::NeighborAdvertisement));
+        assert_eq!(IcmpMessageType::from_i32(4), None);
+    }
 }
